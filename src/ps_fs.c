6774294255d50bf8096b9751a54331fc03c0652b
Christian Korber <ckorber@debian.localdomain>
2024-12-28 20:07:51 +0100

    action: add test scenario
    Signed-off-by: Christian Korber <ckorber@debian.localdomain>

--- src/ps_fs.c
+++ src/ps_fs.c
@@ -14,6 +14,8 @@ void dir_initialize(const char *p) {
     do {
       char path[256];
       char naming[256];
+      char check[256];
+      char *extension;
 
       file = readdir(d);
       if (file){
@@ -28,20 +30,27 @@ void dir_initialize(const char *p) {
         buf = malloc(sizeof(struct stat));
         lstat(path, buf);
         time = localtime(&buf->st_mtime);
+        extension = strrchr(file->d_name, '.');
 
-        snprintf(naming, sizeof(naming),"%s/%d-%d-%d %s",p,
-                 time->tm_year+1900, time->tm_mon, time->tm_mday,
-                 file->d_name);
+        snprintf(check, sizeof(check),"%d-%d-%d_%d:%d:%d_Image",
+                time->tm_year+1900, time->tm_mon, time->tm_mday,
+                 time->tm_hour, time->tm_min, time->tm_sec);
 
-        if(strstr(path, ".jpg") != NULL ||
-        strstr(path, ".jpeg") != NULL ||
-        strstr(path, ".png") != NULL ||
-        strstr(path, ".tiff") != NULL){
-          if (rename(path, naming) == 0){
-            printf("Performed renaming successfully");
-          }
-          else{
-            printf("Error while renaming.");
+        if(strstr(path, check) == NULL && extension != NULL){
+          snprintf(naming, sizeof(naming),"%s/%d-%d-%d_%d:%d:%d_Image%s",p,
+                  time->tm_year+1900, time->tm_mon, time->tm_mday,
+                  time->tm_hour, time->tm_min, time->tm_sec, extension);
+
+          if(strstr(path, ".jpg") != NULL ||
+          strstr(path, ".jpeg") != NULL ||
+          strstr(path, ".png") != NULL ||
+          strstr(path, ".tiff") != NULL){
+            if (rename(path, naming) == 0){
+              printf("Performed renaming successfully\n");
+            }
+            else{
+              printf("Error while renaming.\n");
+            }
           }
         }
 
