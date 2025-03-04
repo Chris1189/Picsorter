568502941cd74d0f97e4f8ab35b4a8ce432e1cc9
Christian Korber <ckorber@debian.localdomain>
2025-03-04 21:43:34 +0100

    picsorter: refactorization
    Signed-off-by: Christian Korber <ckorber@debian.localdomain>

--- src/ps_fs.c
+++ src/ps_fs.c
@@ -1,29 +1,24 @@
 #include "ps_fs.h"
 
 void
+check_abspath (char *dest, const char *path)
+{
+  if (path && *path != '/')
+    {
+      dest = realpath (path, NULL) ? realpath (path, NULL) : NULL;
+    }
+  snprintf (dest, sizeof (path), "%s", path);
+}
+
+void
 dir_initialize (const char *p, int rec, const char *base_dir)
 {
   DIR *d;
   char *file_abs = malloc (sizeof (char) * 256);
   char *base_abs = malloc (sizeof (char) * 256);
 
-  if (p && *p != '/')
-    {
-      file_abs = realpath (p, NULL) ? realpath (p, NULL) : NULL;
-    }
-  else
-    {
-      strcpy (file_abs, p);
-    }
-
-  if (base_dir && *base_dir != '/')
-    {
-      base_abs = realpath (base_dir, NULL) ? realpath (base_dir, NULL) : NULL;
-    }
-  else
-    {
-      strcpy (base_abs, base_dir);
-    }
+  check_abspath (file_abs, p);
+  check_abspath (base_abs, base_dir);
 
   d = opendir (file_abs);
 
@@ -31,15 +26,27 @@ dir_initialize (const char *p, int rec, const char *base_dir)
     {
       ps_scandir (file_abs, d, rec, base_abs);
     }
+
   free (file_abs);
   free (base_abs);
   closedir (d);
 }
 
 void
+perform_dir_recursive (char *dest, const char *path, dirent *file,
+                       const char *base, int rec)
+{
+  if (file->d_type == DT_DIR && rec)
+    {
+      snprintf (dest, sizeof (dest), "%s/%s", path, file->d_name);
+      dir_initialize (dest, rec, base);
+    }
+}
+
+void
 ps_scandir (const char *p, DIR *d, int rec, const char *base)
 {
-  struct dirent *file;
+  dirent *file;
 
   do
     {
@@ -53,23 +60,45 @@ ps_scandir (const char *p, DIR *d, int rec, const char *base)
         {
           continue;
         }
-
-      if (file->d_type == DT_DIR && rec)
-        {
-          snprintf (new_p, sizeof (new_p), "%s/%s", p, file->d_name);
-          dir_initialize (new_p, rec, base);
-        }
+      perform_dir_recursive (new_p, p, file, base, rec);
       ps_rename (p, file, rec, base);
     }
   while (file);
 }
 
 void
-ps_rename (const char *p, struct dirent *file, int rec, const char *base_dir)
+get_stats (const char *path, statf *buf, tm *time, char *extension,
+           dirent *file)
+{
+  buf = malloc (sizeof (statf));
+  lstat (path, buf);
+  time = localtime (&buf->st_mtime);
+  extension = strrchr (file->d_name, '.');
+}
+
+void
+prepare_string (char *dst, const char *str, const char *path, int y, int m,
+                int d, int h, int min, int s, dirent *file, const char *regex,
+                const char *ext)
+{
+  if (path && file)
+    snprintf (dst, sizeof (dst), str, path, y, m, d, y, m, d, h, min, s,
+              file->d_name);
+  else if (path && regex && ext)
+    snprintf (dst, sizeof (dst), str, path, y, m, d, y, m, d, h, min, s, regex,
+              ext);
+  else if (path && !regex && ext)
+    snprintf (dst, sizeof (dst), str, path, y, m, d, y, m, d, h, min, s, ext);
+  else
+    snprintf (dst, sizeof (dst), str, y, m, d, h, min, s);
+}
+
+void
+ps_rename (const char *p, dirent *file, int rec, const char *base_dir)
 {
   int match;
-  struct stat *buf;
-  struct tm *time;
+  statf *buf;
+  tm *time;
   char path[500];
   char naming[500];
   char *regmatch;
@@ -81,14 +110,11 @@ ps_rename (const char *p, struct dirent *file, int rec, const char *base_dir)
   regmatch = malloc (sizeof (char) * 256);
   snprintf (path, sizeof (path), "%s/%s", p, file->d_name);
 
-  buf = malloc (sizeof (struct stat));
-  lstat (path, buf);
-  time = localtime (&buf->st_mtime);
-  extension = strrchr (file->d_name, '.');
+  get_stats (path, buf, time, extension, file);
 
-  snprintf (check, sizeof (check), date, time->tm_year + 1900,
-            time->tm_mon + 1, time->tm_mday, time->tm_hour, time->tm_min,
-            time->tm_sec);
+  prepare_string (check, date, NULL, time->tm_year + 1900, time->tm_mon + 1,
+                  time->tm_mday, time->tm_hour, time->tm_min, time->tm_sec,
+                  NULL, NULL, NULL);
 
   match = ps_test_naming (file->d_name, regmatch);
 
@@ -97,29 +123,28 @@ ps_rename (const char *p, struct dirent *file, int rec, const char *base_dir)
     {
       if (match == 0)
         {
-          snprintf (naming, sizeof (naming), path_name, rec ? base_dir : p,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_hour, time->tm_min, time->tm_sec, file->d_name);
+          prepare_string (naming, path_name, rec ? base_dir : p,
+                          time->tm_year + 1900, time->tm_mon + 1,
+                          time->tm_mday, time->tm_hour, time->tm_min,
+                          time->tm_sec, file, NULL, NULL);
         }
 
       if (match == 2)
         {
-          snprintf (naming, sizeof (naming), path_name, rec ? base_dir : p,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_hour, time->tm_min, time->tm_sec, regmatch,
-                    extension);
+          prepare_string (naming, path_name, rec ? base_dir : p,
+                          time->tm_year + 1900, time->tm_mon + 1,
+                          time->tm_mday, time->tm_hour, time->tm_min,
+                          time->tm_sec, NULL, regmatch, extension);
         }
 
       free (regmatch);
 
       if (match == 3)
         {
-          snprintf (naming, sizeof (naming), path_name, rec ? base_dir : p,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_year + 1900, time->tm_mon + 1, time->tm_mday,
-                    time->tm_hour, time->tm_min, time->tm_sec, extension);
+          prepare_string (naming, path_name, rec ? base_dir : p,
+                          time->tm_year + 1900, time->tm_mon + 1,
+                          time->tm_mday, time->tm_hour, time->tm_min,
+                          time->tm_sec, NULL, NULL, extension);
         }
 
       if (strstr (path, ".jpg") != NULL || strstr (path, ".jpeg") != NULL
