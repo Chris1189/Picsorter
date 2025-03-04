f493578d7cbf835501511984b557988ce8ff71fa
Christian Korber <ckorber@debian.localdomain>
2025-03-04 21:43:34 +0100

    picsorter: refactorization
    Signed-off-by: Christian Korber <ckorber@debian.localdomain>

--- src/ps_fs.h
+++ src/ps_fs.h
@@ -4,10 +4,20 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
+typedef struct dirent dirent;
+typedef struct stat statf;
+typedef struct tm tm;
+
 void dir_initialize (const char *path, int rec, const char *base_dir);
+void perform_dir_recursive (char *dest, const char *path, dirent *file,
+                            const char *base, int rec);
+void get_stats (const char *path, statf *buf, tm *time, char *extension,
+                dirent *file);
+void prepare_string (char *dst, const char *str, const char *path, int y,
+                     int m, int d, int h, int min, int s, dirent *file,
+                     const char *regex, const char *ext);
 void ps_scandir (const char *p, DIR *d, int rec, const char *base_dir);
-void ps_rename (const char *p, struct dirent *file, int rec,
-                const char *base_dir);
+void ps_rename (const char *p, dirent *file, int rec, const char *base_dir);
 int ps_create (const char *p, int y, int m, int d);
 int ps_test_naming (const char *p, char *buf);
 
